31f5838493cf0407664b12864f9d688bde5bb929
Aron Bustos <ar.bustos@duocuc.cl>
2024-06-05 22:29:51 -0400

    he agregado scanner para interactuar con el cliente (aunque no era parte de los requerimientos)

--- DesafioManejoExcepciones/ManejoExcepciones/src/Main.java
+++ DesafioManejoExcepciones/ManejoExcepciones/src/Main.java
@@ -1,21 +1,38 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       // Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        
+
         //System.out.printf("Hello and welcome!");
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
 
+        // Pedir datos al usuario
+        System.out.print("Nombre de venta aca --->  ");
+        String nombreVenta = sc.nextLine();
+
+        System.out.print("fecha de la venta (formato DDMMYYYY) aca --->: ");
+        String fechaVenta = sc.nextLine();
+
+        System.out.print("Ingrese la patente del vehiculo aca --->: ");
+        String patenteVehiculo = sc.nextLine();
+
+        System.out.print("edad del cliente aca --->: ");
+        int edadCliente = sc.nextInt();
+
+
         // Creacion de objeto Cliente y Vehiculo
-        Cliente cliente = new Cliente(30);  // Edad del cliente como parámetro
-        Vehiculo vehiculo = new Vehiculo("ABC123");  // Patente del vehículo como parámetro
+        Cliente cliente = new Cliente(edadCliente);
+        Vehiculo vehiculo = new Vehiculo(patenteVehiculo);
 
-        LibroVenta libroVenta = new LibroVenta("Venta1", "01102020");
+        LibroVenta libroVenta = new LibroVenta(nombreVenta, fechaVenta);
 
+        // Guardar la venta
         libroVenta.guardarVenta(cliente, vehiculo);
+
+        sc.close();
     }
 }
